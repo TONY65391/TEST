183f663e05c73d845ecdbf5e266a3f11895a319b
TONY65391 <tonyanthony65391@gmail.com>
2025-10-05 19:14:57 +0100

    lighter

--- APP/static/app.js
+++ APP/static/app.js
@@ -9,10 +9,12 @@ const pictureInput = document.getElementById('id_Profile_Picture');
 const error_list = document.querySelector('.error .cont ol');
 const errorBtn = document.querySelector('.error .cont .options button');
 
-errorBtn.addEventListener('click', () => {
-    document.querySelector('.error').classList.remove('show');
-    document.querySelector('.error').classList.add('hidden');
-})
+if (errorBtn){
+    errorBtn.addEventListener('click', () => {
+        document.querySelector('.error').classList.remove('show');
+        document.querySelector('.error').classList.add('hidden');
+    })
+}
 
 menuBtn.onclick = () => {
     menuBtn.classList.toggle('open');
@@ -26,100 +28,101 @@ menuBtn.onclick = () => {
 }
 
 const allInputs = [...document.querySelectorAll('form input')];
+if (form){
+    form.addEventListener('submit', (e) => {
 
-form.addEventListener('submit', (e) => {
+        if (emailInput){
+            error = Signup(usernameInput.value, emailInput.value, passwordInput.value);
+            if (error.length > 0){
+                e.preventDefault();
+                document.querySelector('.error').classList.add('show');
+                document.querySelector('.error').classList.remove('hidden');
 
-    if (emailInput){
-        error = Signup(usernameInput.value, emailInput.value, passwordInput.value);
-        if (error.length > 0){
-            e.preventDefault();
-            document.querySelector('.error').classList.add('show');
-            document.querySelector('.error').classList.remove('hidden');
-
-            error_list.innerHTML = "";
-            for (i = 0; i < error.length; i++){
-                const li = document.createElement('li');
-                li.innerHTML = `<i class="fa-solid fa-triangle-exclamation"></i> ${error[i]}`;
-                error_list.append(li);
+                error_list.innerHTML = "";
+                for (i = 0; i < error.length; i++){
+                    const li = document.createElement('li');
+                    li.innerHTML = `<i class="fa-solid fa-triangle-exclamation"></i> ${error[i]}`;
+                    error_list.append(li);
+                }
             }
         }
-    }
-    else{
-        error = Login(usernameInput.value, passwordInput.value);
-        if (error.length > 0){
-            e.preventDefault();
-            document.querySelector('.error').classList.add('show');
-            document.querySelector('.error').classList.remove('hidden');
+        else{
+            error = Login(usernameInput.value, passwordInput.value);
+            if (error.length > 0){
+                e.preventDefault();
+                document.querySelector('.error').classList.add('show');
+                document.querySelector('.error').classList.remove('hidden');
 
-            error_list.innerHTML = "";
-            for (i = 0; i < error.length; i++){
-                const li = document.createElement('li');
-                li.innerHTML = `<i class="fa-solid fa-triangle-exclamation"></i> ${error[i]}`;
-                error_list.append(li);
+                error_list.innerHTML = "";
+                for (i = 0; i < error.length; i++){
+                    const li = document.createElement('li');
+                    li.innerHTML = `<i class="fa-solid fa-triangle-exclamation"></i> ${error[i]}`;
+                    error_list.append(li);
+                }
             }
         }
-    }
 
-    function Signup(username, email, password){
+        function Signup(username, email, password){
 
-        var error = []
-        if (username === password){
-            error.push("Username and password must not be similar");
+            var error = []
+            if (username === password){
+                error.push("Username and password must not be similar");
+            };
+            if (String(username).includes('@') == false){
+                error.push("Username must contain the '@' symbol")
+            };
+            if (String(username).includes(' ')){
+                error.push("Username must not include space")
+            };
+            if (String(email).includes('@') == false){
+                error.push("Email must contain the '@' symbol")
+            };
+            if (String(password).includes('@')){
+                error.push("Password must not contain the '@' symbol")
+            };
+            if (String(password).includes(' ')){
+                error.push("Password must not include space");
+            };
+            if (password.length < 8){
+                error.push("Password must contain at least 8 characters")
+            };
+            if (username.length < 10){
+                error.push("Username must contain at least 10 characters");
+            };
+            if (password.length < 10){
+                error.push("Password must contain at least 10 characters");
+            };
+            
+            return error;
         };
-        if (String(username).includes('@') == false){
-            error.push("Username must contain the '@' symbol")
-        };
-        if (String(username).includes(' ')){
-            error.push("Username must not include space")
-        };
-        if (String(email).includes('@') == false){
-            error.push("Email must contain the '@' symbol")
-        };
-        if (String(password).includes('@')){
-            error.push("Password must not contain the '@' symbol")
-        };
-        if (String(password).includes(' ')){
-            error.push("Password must not include space");
-        };
-        if (password.length < 8){
-            error.push("Password must contain at least 8 characters")
-        };
-        if (username.length < 10){
-            error.push("Username must contain at least 10 characters");
-        };
-        if (password.length < 10){
-            error.push("Password must contain at least 10 characters");
-        };
-        
-        return error;
-    };
 
-    function Login(username, password){
-        var error = [];
-        if (username === password){
-            error.push("Username and password must not be similar");
+        function Login(username, password){
+            var error = [];
+            if (username === password){
+                error.push("Username and password must not be similar");
+            };
+            if (password.length < 8){
+                error.push("Password must contain at least 8 characters")
+            };
+            if (username.length < 10){
+                error.push("Username must contain at least 10 characters");
+            };
+            if (password.length < 10){
+                error.push("Password must contain at least 10 characters");
+            };
+            if (String(password).includes('@')){
+                error.push("Password must not contain the '@' symbol")
+            };
+            if (String(password).includes(' ')){
+                error.push("Password must not include space");
+            };
+            if (String(username).includes('@') == false){
+                error.push("Username must contain the '@' symbol")
+            };
+            if (String(username).includes(' ')){
+                error.push("Username must not include space")
+            };
+            return error;
         };
-        if (password.length < 8){
-            error.push("Password must contain at least 8 characters")
-        };
-        if (username.length < 10){
-            error.push("Username must contain at least 10 characters");
-        };
-        if (password.length < 10){
-            error.push("Password must contain at least 10 characters");
-        };
-        if (String(password).includes('@')){
-            error.push("Password must not contain the '@' symbol")
-        };
-        if (String(password).includes(' ')){
-            error.push("Password must not include space");
-        };
-        if (String(username).includes('@') == false){
-            error.push("Username must contain the '@' symbol")
-        };
-        if (String(username).includes(' ')){
-            error.push("Username must not include space")
-        };
-        return error;
-    };
-});
+    });
+}
